4dce6e6afbff855e3cb7704ca53fbadc3b164616
Magda <ma.wernik@gmail.com>
2023-01-12 15:14:00 +0100

    delete function done

--- src/api/tasks.js
+++ src/api/tasks.js
@@ -3,3 +3,14 @@ import axios from 'axios';
 export default axios.create({
     baseURL:'http://localhost:3006/',
 })
+async function getTasks() {
+    const response = await axios.get('http://localhost:3006/tasks');
+    return response.data
+};
+
+async function deleteTask(id) {
+    const response = await axios.delete(`http://localhost:3006/tasks/${id}`);
+    return response 
+}
+
+export {deleteTask, getTasks};
--- src/components/List.js
+++ src/components/List.js
@@ -1,22 +1,26 @@
 import { useEffect, useState } from "react"
 import React from 'react'
-import axios from "axios"
 import trash from '../assets/images/trash.svg';
 import pen from '../assets/images/pen.svg';
 import '../styles/List.css';
+import { deleteTask, getTasks } from "../api/tasks";
 
 function List() {
     const [tasks,setTasks] = useState([])
+    async function showTasks() {
+        const allTasks = await getTasks()
+        setTasks(allTasks)
+    }
     useEffect(() => {
-        axios.get('http://localhost:3006/tasks')
-        .then(res => {
-            console.log(res)
-            setTasks(res.data)
-        })
-        .catch(err=> {
-            console.log(err)
-        })
+        showTasks()
     }, [])
+
+    const deleteData = async (id) => {
+       await deleteTask(id)
+       await showTasks();
+        };
+
+    
   return (
     
         <>
@@ -25,8 +29,8 @@ function List() {
              tasks.map((tasks,index) =>
             
                  <li key={index}>{tasks.task}
-                 <img src= {trash} id='trash-icon' alt='trash icon'/>
-                 <img src= {pen} id='pen-icon' alt='pen icon'/>
+                 <button id="delete-button"  onClick={() => deleteData(tasks.id)}><img src= {trash} id='trash-icon' alt='trash icon'/></button>
+                 <button id="modify-button"><img src= {pen} id='pen-icon' alt='pen icon'/></button>
                  </li>
             
              )
