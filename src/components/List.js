b455081adefb0d2a7a6062b364276438e2a42bca
Magda <ma.wernik@gmail.com>
2023-01-12 13:02:14 +0100

    icons added

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react"
 import React from 'react'
 import axios from "axios"
+import trash from '../assets/images/trash.svg';
+import pen from '../assets/images/pen.svg';
+import '../styles/List.css';
 
 function List() {
     const [tasks,setTasks] = useState([])
@@ -15,20 +18,22 @@ function List() {
         })
     }, [])
   return (
-    <div>
+    
         <>
-        <ul className="list">
+             <ul>
             {
              tasks.map((tasks,index) =>
-             <>
+            
                  <li key={index}>{tasks.task}
+                 <img src= {trash} id='trash-icon' alt='trash icon'/>
+                 <img src= {pen} id='pen-icon' alt='pen icon'/>
                  </li>
-             </>
+            
              )
             }
-        </ul>
+             </ul>
         </>
-    </div>
+    
   )
 }
 
